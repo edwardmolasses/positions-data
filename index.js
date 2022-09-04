0b6fd77bfcef73b795298b0f9b9833d89af33cb2
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-09-04 14:20:51 -0700

    add react client

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5003
+// const path = require('path')
+const PORT = 5003
 const app = express(); //Line 2
+const path = __dirname + '/public/views/';
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
+app.use(express.static(path));
+// app.get('/', (req, res) => res.render('index.html'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path + "index.html");
+});
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
