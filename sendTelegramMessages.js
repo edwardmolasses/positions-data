7f51f382f90d778447f23a9408760a09afcba89f
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-11-16 16:37:42 -0800

    reset send telegram functions to previous working

--- sendTelegramMessages.js
+++ sendTelegramMessages.js
@@ -1,9 +1,7 @@
 const { getAlertMessage, getDailyDigestMessage } = require('./buildMessage');
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
-const { chromium } = require("playwright");
-// const puppeteer = require("puppeteer");
-
+const puppeteer = require("puppeteer");
 const { DEBUG_MODE } = require('./constants');
 
 const isDebugMode = DEBUG_MODE['EXTREME_LONGS'] || DEBUG_MODE['HOURLY'] || DEBUG_MODE['EXTREME_SHORTS'] || DEBUG_MODE['LOW_TF_LEVERAGE'];
@@ -12,7 +10,7 @@ const TG_API_ID = parseInt(process.env.TG_API_ID);
 const TG_API_HASH = process.env.TG_API_HASH;
 const TG_AUTH_KEY = isDebugMode ? process.env.TG_AUTH_KEY : process.env.TG_BOT_AUTH_KEY;
 
-const remoteChartUrl = 'https://floating-hamlet-81093.herokuapp.com/';
+const remoteChartUrl = 'floating-hamlet-81093.herokuapp.com';
 const remoteChartWidth = 1030;
 const remoteChartHeight = 675;
 const chartFilename = 'chart.png';
@@ -39,37 +37,24 @@ async function sendTelegramDailyMessage() {
 
 async function sendTelegramAlertMessage() {
     const msg = await getAlertMessage();
+    puppeteer
+        .launch({
+            defaultViewport: {
+                width: remoteChartWidth,
+                height: remoteChartHeight,
+            },
+        })
+        .then(async (browser) => {
+            const page = await browser.newPage();
+            const url = `http://${remoteChartUrl}`;
 
-    await sendMsgByBot(msg);
-    // let browser = await chromium.launch();
-    // let page = await browser.newPage();
-
-    // await page.setViewportSize({ width: remoteChartWidth, height: remoteChartHeight });
-    // await page.goto(remoteChartUrl);
-    // setTimeout(async function () {
-    //     await page.screenshot({ path: 'chart.png' });
-    //     await browser.close();
-    //     await sendMsgByBot(msg);
-    // }, 10000);
-
-    // puppeteer
-    //     .launch({
-    //         defaultViewport: {
-    //             width: remoteChartWidth,
-    //             height: remoteChartHeight,
-    //         },
-    //     })
-    //     .then(async (browser) => {
-    //         const page = await browser.newPage();
-    //         const url = `http://${remoteChartUrl}`;
-
-    //         await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 });
-    //         setTimeout(async function () {
-    //             await page.screenshot({ path: chartFilename });
-    //             await browser.close();
-    //             await sendMsgByBot(msg);
-    //         }, 10000);
-    //     });
+            await page.goto(url, { waitUntil: 'networkidle0', timeout: 0 });
+            setTimeout(async function () {
+                await page.screenshot({ path: chartFilename });
+                await browser.close();
+                await sendMsgByBot(msg);
+            }, 10000);
+        });
 }
 
 module.exports = {
