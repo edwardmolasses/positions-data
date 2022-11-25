14d91d0c04b52fb94c21c105f89a873fe0fa39b3
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-11-25 12:22:02 -0800

    disable telegram msgs remote

--- constants.js
+++ constants.js
@@ -19,7 +19,7 @@ module.exports = {
         EXTREME_LONGS: false,
         EXTREME_SHORTS: false,
         LOW_TF_LEVERAGE: false,
-        TELEGRAM_TOGGLE: true,
+        TELEGRAM_TOGGLE: false,
         GIRAFFLE_MODE: true,
     },
     ACCEPT_STRINGS: {
